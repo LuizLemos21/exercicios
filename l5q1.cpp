5425c39459b0dae63db32a314231fe8dd9cf1f0c
LuizLemos21 <104809823+LuizLemos21@users.noreply.github.com>
2022-09-01 20:30:55 -0300

    Create l5q1.cpp

---
+++ l5q1.cpp
@@ -0,0 +1,13 @@
+/* 
+ LISTA 05 PROF: CRISTIANO FUSCHILO 
+ LUIZ FERNANDO SILVA LEMOS 
+ QUESTÃO 1. 
+ Faça um programa que leia uma string e a imprima. 
+*/
+#include <stdio.h>
+#include <string.h>
+int main() {
+ char str[50];
+ gets(str);
+ printf("%s", str);
+}
